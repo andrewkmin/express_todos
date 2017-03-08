ed70d358daee53a16852d58b2e538414323b2af8
andrewkmin <andrewmin8@gmail.com>
2017-03-08 13:10:14 -0800

    another chnage

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 })
 
-app.listen(process.env.port || 3000, function() {
-  console.log('running at port: 3000')
+app.listen(process.env.PORT || 3000, function() {
+  console.log('running at port: ' process.env.PORT || '3000')
 })
